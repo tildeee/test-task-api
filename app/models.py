531be1ef79a8b948748f2796e8c9ae05031ef594
Kaida Masaki <kaida@adadevelopersacademy.org>
2021-02-24 09:04:23 -0800

    Working serialization

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
-from flask import current_app
+from flask import current_app, json
 from datetime import datetime
 from app import db
+
+class ModelEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, db.Model):
+            return obj.data()
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+
+class Task(db.Model):
+    task_id = db.Column(db.Integer, primary_key=True)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+    updated_on = db.Column(
+        db.DateTime(), default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    completed_at = db.Column(db.DateTime, nullable=True)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
+
+    def data(self):
+        return {
+            "id": self.task_id,
+            "description": self.description,
+            "is_complete": bool(self.completed_at),
+            "title": self.title
+        }
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,29 @@
 from flask import Flask, Blueprint, request, jsonify, redirect, url_for
-from app.models import Task, Goal
+from app.models import ModelEncoder, Task#, Goal
 from app import db
 from datetime import datetime
 
+import app
 import os
 import requests
 
 task_page = Blueprint('task_page', __name__)
+task_page.json_encoder = ModelEncoder
+
+@task_page.route('/')
+def index():
+    return {
+        "name": "Kaida",
+        "message": "Hello, world!"
+    }
+
+@task_page.route('/tasks', methods=['GET', 'POST'])
+def tasks():
+    if request.method == 'GET':
+        return jsonify(Task.query.all())
+    elif request.method == 'POST':
+        task = Task(**request.json)
+        db.session.add(task)
+        db.session.commit()
+
+        return jsonify({"task": task})
