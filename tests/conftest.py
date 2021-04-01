40bbc95f6b819012e2cdcf207cd0624d6188ee50
simon <simon@adadevelopersacademy.org>
2021-04-01 08:44:32 -0700

    test db teardown, one_task fixture, first tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ import os
 # this import below didn't work until i had an __init__ in here, i knew that bc this error
 # ImportError: attempted relative import with no known parent package
 from app import create_app
+from app.models.task import Task
+from app import db
 
 # Need to include that conftest is a Flask Thing
 
@@ -21,9 +23,19 @@ def app():
     # close and remove the temporary database
     os.close(db_fd)
     os.unlink(db_path)
+    with app.app_context():
+        db.drop_all()
 
 
 @pytest.fixture
 def client(app):
     """A test client for the app."""
     return app.test_client()
+
+
+@pytest.fixture
+def one_task(app):
+    new_task = Task(
+        title="hi", description="bye", completed_at=None)
+    db.session.add(new_task)
+    db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -10,14 +10,26 @@ def test_index(client):
 
 
 def test_get_tasks_no_saved_tasks(client):
-    response = client.get("/tasks/1")
+    response = client.get("/tasks")
 
-    assert response.status_code == 404
+    assert response.status_code == 200
 
     response_body = response.get_json()
 
     assert response_body == []
 
+
+def test_get_tasks_one_saved_tasks(client, one_task):
+    response = client.get("/tasks")
+    response_body = response.get_json()
+    first_task = response_body[0]
+
+    assert response.status_code == 200
+
+    assert "id" in first_task
+    assert "title" in first_task
+    assert "description" in first_task
+    assert "is_complete" in first_task
 
 # def test_list_tasks(client):
 #     response = client.get("/tasks")
