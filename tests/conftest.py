9e865958e9bbda3b120dfef6844b35b0064b997c
simon <simon@adadevelopersacademy.org>
2021-04-06 13:31:29 -0700

    cleans up conftest, tests on crud routes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,41 +1,30 @@
-import tempfile
 import pytest
-import os
-# this import below didn't work until i had an __init__ in here, i knew that bc this error
-# ImportError: attempted relative import with no known parent package
 from app import create_app
 from app.models.task import Task
 from app import db
 
-# Need to include that conftest is a Flask Thing
-
 
 @pytest.fixture
 def app():
-    """Create and configure a new app instance for each test."""
-    # create a temporary file to isolate the database for each test
-    db_fd, db_path = tempfile.mkstemp()
-    # create the app with common test config
+    # create the app with a test config dictionary
     app = create_app({"TESTING": True})
 
-    yield app
+    with app.app_context():
+        yield app
 
     # close and remove the temporary database
-    os.close(db_fd)
-    os.unlink(db_path)
     with app.app_context():
         db.drop_all()
 
 
 @pytest.fixture
 def client(app):
-    """A test client for the app."""
     return app.test_client()
 
 
 @pytest.fixture
 def one_task(app):
     new_task = Task(
-        title="hi", description="bye", completed_at=None)
+        title="Go on my daily walk", description="Notice something new every day", completed_at=None)
     db.session.add(new_task)
     db.session.commit()
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,7 +1,3 @@
-from flask import url_for
-from app.routes import task_page
-
-
 def test_index(client):
     response = client.get("/")
     response_body = response.get_json()
@@ -31,6 +27,92 @@ def test_get_tasks_one_saved_tasks(client, one_task):
     assert "description" in first_task
     assert "is_complete" in first_task
 
-# def test_list_tasks(client):
-#     response = client.get("/tasks")
-#     print("GGGGGGGGGGGGGGGG", response.data)
+
+def test_get_task(client, one_task):
+    response = client.get("/tasks/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert "id" in response_body["task"]
+    assert "title" in response_body["task"]
+    assert "description" in response_body["task"]
+    assert "is_complete" in response_body["task"]
+
+
+def test_get_task_not_found(client):
+    response = client.get("/tasks/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == None
+
+
+def test_post_task(client):
+    response = client.post("/tasks", json={
+        "title": "c",
+        "description": "Test Description",
+        "completed_at": None
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+
+    assert "task" in response_body
+    assert response_body["task"] == {
+        "description": "Test Description",
+        "id": 1,
+        "is_complete": False,
+        "title": "c"
+    }
+
+
+def test_update_task(client, one_task):
+    response = client.put("/tasks/1", json={
+        "title": "c",
+        "description": "Test Description",
+        "completed_at": None
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert "task" in response_body
+    assert response_body["task"] == {
+        "description": "Test Description",
+        "id": 1,
+        "is_complete": False,
+        "title": "c"
+    }
+
+
+def test_update_task_not_found(client):
+    response = client.put("/tasks/1", json={
+        "title": "c",
+        "description": "Test Description",
+        "completed_at": None
+    })
+
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+
+    assert response_body == None
+
+
+def test_delete_task(client, one_task):
+    response = client.delete("/tasks/1")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert "details" in response_body
+
+    # Check that the task was deleted
+    response = client.get("/tasks/1")
+    assert response.status_code == 404
+
+def test_delete_task_not_found(client):
+    response = client.delete("/tasks/1")
+
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == None
